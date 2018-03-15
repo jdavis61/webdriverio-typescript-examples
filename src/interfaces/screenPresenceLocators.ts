03b8bad74aae4f69dc4fb156444715fe3f44a02d
James Davis <jrdavismaine@gmail.com>
2018-03-14 22:08:24 -0400

    Removed interface

--- src/interfaces/screenPresenceLocators.ts
+++
@@ -1,8 +0,0 @@
-export interface ScreenPresenceLocators {
-    login: string;
-    securedPage: string;
-    checkboxes: string;
-    hovers: string;
-    userprofile: string;
-    javascriptAlertPage: string;    
-}
