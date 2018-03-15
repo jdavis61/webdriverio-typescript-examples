2dc57320b1e2f511ba656be247b05d6edd2ca0c0
James Davis <jrdavismaine@gmail.com>
2018-03-15 06:14:09 -0400

    Rename test

---
+++ src/pages/yoddddd.ts
@@ -0,0 +1,45 @@
+var locators = require('../selectors/basePage.json');
+
+export default class BasePage {
+    public get subheader() {
+        return browser.$(locators.pageSubHeaderLocator);
+    }
+
+    public get footerText() {
+        return browser.$(locators.footerTextLocator);
+    }
+
+    public get pageMessage() {
+        return browser.$(locators.pageMessageLocator);
+    }
+
+    public get closeButton() {
+        return browser.$(locators.closeButton);
+    }
+
+    public getPageHeader() {
+        let h2Selector: string = 'div.example h2';
+        let h3Selector: string = 'div.example h3';
+        if (browser.$$(locators.h2Selector).length > 0) {
+            return $(h2Selector).getText();
+        }
+        return browser.$(h3Selector).getText();
+    }
+
+    public getPageSubHeader() {
+        return this.subheader.getText();
+    }
+
+    public getFooterText() {
+        return this.footerText.getText();
+    }
+
+    public getPageMessage() {
+        return this.pageMessage.getText().split('\n')[0].trim();
+    }
+
+    public closePageMessage() {
+        this.closeButton.click();
+        browser.waitUntil(() => !this.closeButton.isVisible());
+    }
+}
