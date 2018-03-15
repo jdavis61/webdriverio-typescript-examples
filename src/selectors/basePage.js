4e4df76ff121ed3366aec4b5e13a55cfabd8a75a
James Davis <jrdavismaine@gmail.com>
2018-03-15 06:15:25 -0400

    Removing renamed files from commit

--- src/selectors/basePage.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    pageSubHeaderLocator: '.subheader',
-    footerTextLocator: '#page-footer',
-    pageMessageLocator: '.flash',
-    h2Selector: 'div.example h2',
-    h3Selector: 'div.example h3',
-    closeButton: '#flash > .close',
-};
