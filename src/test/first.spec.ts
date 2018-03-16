9f2b34a3441ed4ffa6b53ac42c22f36403736d04
James Davis <jrdavismaine@gmail.com>
2018-03-16 06:33:41 -0400

    Corrected indent

--- src/test/first.spec.ts
+++ src/test/first.spec.ts
@@ -2,11 +2,10 @@ import Google from '../pages/Google';
 
 describe('Google basic search', () => {
 
-  it('Test',() => {
-    let google : Google = new Google();
-    google.gotoHomePage();
-    google.enterData('webdriver.io typescript');
-    browser.pause(1000);
-  })
-
+    it('Test',() => {
+        let google : Google = new Google();
+        google.gotoHomePage();
+        google.enterData('webdriver.io typescript');
+        browser.pause(1000);
+    });
 });
