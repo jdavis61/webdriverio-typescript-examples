52efbdb04dc77ea72f4a263181a8f2d2857d9af5
James Davis <jrdavismaine@gmail.com>
2018-03-16 06:33:26 -0400

    Added JavaScript Alert test

---
+++ src/pages/JavaScriptAlerts.ts
@@ -0,0 +1,36 @@
+import BasePage from './BasePage';
+var selectors = require('../selectors/javascriptAlerts.json');
+
+export default class JavaScriptAlertsPage extends BasePage {
+    constructor() {
+        super();
+    }
+
+    get jsAlertButton() {
+        return browser.$(selectors.jsAlertButtonLocator);
+    }
+
+    get jsConfirmButton() {
+        return browser.$(selectors.jsConfirmButtonLocator);
+    }
+
+    get result() {
+        return browser.$(selectors.resultLocator);
+    }
+
+    clickJSAlertButton() {
+        this.jsAlertButton.click();
+    }
+
+    clickJSConfirmButton() {
+        this.jsConfirmButton.click();
+    }
+
+    getResult() {
+        return this.result.getText().trim();
+    }
+
+    closeAlert() {
+        browser.alertAccept();
+    }
+}
---
+++ src/test/javascriptAlertTest.spec.ts
@@ -0,0 +1,29 @@
+import { assert } from 'chai';
+import NavigationMenu from '../pages/NavigationMenu';
+import JavaScriptAlertsPage from '../pages/JavascriptAlerts';
+
+describe('JavaScript Alert test', () => {
+
+    let jsAlert : JavaScriptAlertsPage;
+    before('setup', () => {
+        jsAlert = new JavaScriptAlertsPage();
+        let navMenu: NavigationMenu = new NavigationMenu();
+        navMenu.loadNavigationMenu();
+        navMenu.clickJavaScriptAlertsPage();
+    });
+
+    it('JavaScript Alert test', () => {
+        assert.equal(jsAlert.getPageHeader(), 'JavaScript Alerts');
+        jsAlert.clickJSAlertButton();
+        jsAlert.closeAlert();
+        assert.equal(jsAlert.getResult(), 'You successfuly clicked an alert');
+    });
+
+    it('JavaScript confirm test', () => {
+        assert.equal(jsAlert.getPageHeader(), 'JavaScript Alerts');
+        jsAlert.clickJSConfirmButton();
+        jsAlert.closeAlert();
+        assert.equal(jsAlert.getResult(), 'You clicked: Ok');
+    });
+});
+
