53d242821fe182148b9fa2af7ce4a9787db9f591
James Davis <jrdavismaine@gmail.com>
2018-03-17 12:07:36 -0400

    Fixed errors

--- src/test/hoverstest.spec.ts
+++ src/test/hoverstest.spec.ts
@@ -5,12 +5,14 @@ import HoversPage from '../pages/HoversPage';
 describe('Hovers test', () => {
     it('Hover over image test', () => {
         for (let i = 0; i < 3; i += 1) {
-            NavigationMenu.loadNavigationMenu();
-            NavigationMenu.clickHoversPage();
-            HoversPage.hoverOverImage(i);
-            assert.equal(HoversPage.getUserName(i), `user${i + 1}`);
-            HoversPage.clickViewProfile(i);
-            HoversPage.returnToHoversPage();
+            const navMenu = new NavigationMenu();
+            const hoversPage = new HoversPage();
+            navMenu.loadNavigationMenu();
+            navMenu.clickHoversPage();
+            hoversPage.hoverOverImage(i);
+            assert.equal(hoversPage.getUserName(i), `user${i + 1}`);
+            hoversPage.clickViewProfile(i);
+            hoversPage.returnToHoversPage();
         }
     });
 });
