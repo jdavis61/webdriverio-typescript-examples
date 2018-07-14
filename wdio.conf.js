6d9b4a6f76fd307addf189528b057e96dabe1acd
James Davis <jrdavismaine@gmail.com>
2018-07-14 07:58:14 -0400

    Updated config files, added build script

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './src/test/**/*.ts'
+        './lib/test/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
